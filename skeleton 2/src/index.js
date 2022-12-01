58b32a2c857bf56397f93294d2d1ae65e3043d81
Andrea Cano <andreacanogisbert@gmail.com>
2022-11-30 17:32:15 -0800

    almot finish ttt

--- skeleton 2/src/index.js
+++ skeleton 2/src/index.js
@@ -1,6 +1,15 @@
-const View = // require appropriate file
-const Game = // require appropriate file
-
+//const View = require('ttt-view.js')
+import View from './ttt-view.js'
+// const Game =  import //require('../ttt_node/game.js')
+import Game from '../ttt_node/game.js'
 document.addEventListener("DOMContentLoaded", () => {
   // Your code here
+  const game = new Game()
+  let el = document.querySelector('.ttt')
+  const view = new View(game, el)
+  
+
+
+
+  
 });
--- skeleton 2/src/ttt-view.js
+++ skeleton 2/src/ttt-view.js
@@ -1,13 +1,46 @@
 class View {
-  constructor(game, el) {}
+  constructor(game, el) {
+    this.game = game;
+    this.el = el;
 
-  setupBoard() {}
+    this.setupBoard()
+    this.bindEvents()
+
+  }
+
+  setupBoard() {
+
+    const ul = document.createElement('ul')
+    
+    for (let i = 0; i < 3; i++) {
+      for (let j = 0; j < 3; j++) {
+        let banana = document.createElement('li')
+        banana.dataset.pos = JSON.stringify([i, j])
+        ul.append(banana)
+      }
+    }
+
+    this.el.append(ul)
+
+  }
   
-  bindEvents() {}
+  bindEvents() {
+    this.el.addEventListener('click', this.handleClick.bind(this))
+  }
 
-  handleClick(e) {}
+  handleClick(e) {
 
-  makeMove(square) {}
+    const el = e.target 
+    this.makeMove(el)
+
+  }
+
+  makeMove(square) {
+    //console.log(JSON.parse(square.dataset.pos))
+    const sq = JSON.parse(square.dataset.pos)
+    this.game.playMove(sq)
+    square.innerHTML = this.game.currentPlayer
+  }
 
 }
 
